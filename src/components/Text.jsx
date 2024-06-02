42171ea188eff7c1cd4eebee84276667c1dada1b
sanketauti8 <sanketauti8@gmail.com>
2024-06-02 11:47:49 -0700

    Pushing bug fix code

--- src/components/Text.jsx
+++ src/components/Text.jsx
@@ -7,11 +7,14 @@ const Text = () => {
         const [editMode,setEditMode]=useState(false);
         const [val,setVal]=useState("Double Click to Edit");
 
+        const handleDoubleClick = () => {
+            setEditMode(true);
+          };
 
   return (
     <Draggable>
     {editMode?<input onDoubleClick={(e)=>setEditMode(false)} value={val} onChange={(e)=>setVal(e.target.value)}/>:
-    <h1 onDoubleClick={e=>setEditMode(true)}>{val}</h1>}
+    <h1 onDoubleClick={handleDoubleClick} onTouchStart={handleDoubleClick}>{val}</h1>}
     </Draggable>
   )
 }
